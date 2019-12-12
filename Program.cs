192d3561faee6575b3d7711c80e6761b12b516b5
Justin Swiger <justin.swiger@me.com>
2019-12-12 13:54:02 -0500

    added todos

--- Program.cs
+++ Program.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿// DONE add intro
+// TODO store list of dinos in park
+// TODO add properties for each dino:
+//      name
+//      DietType = carnivore or herbivore
+//      DateAcquired = Default
+//      Weight = in lbs
+//      EnclosureNumber = pen dino is in
+// TODO store dinos in List<Dinosaur>
+// TODO let user view all dinos ordered by DateAcquired
+// TODO let user add a new dino
+// TODO let user remove a dino by name
+// TODO let user transfer a dino to a new pen
+// TODO view the 3 heaviest dinos
+// TODO let user view how many dinos of each DietType
+// TODO let user quit program
+
+using System;
 
 namespace welcome_to_jurassic_park
 {
@@ -6,8 +23,14 @@ namespace welcome_to_jurassic_park
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
-      Console.WriteLine("Hello World!");
+      Console.WriteLine("Welcome to Jurassic Park!");
+      Console.WriteLine("Hold onto your butts.");
+      var input = "";
+      while (input != "quit")
+      {
+        Console.WriteLine();
+        Console.WriteLine();
+      }
     }
   }
 }
